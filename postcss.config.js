526e3f02bf180d12226d09651cfa25c7a65338e8
Luis Angel Perez Melendez <angelmelen88@gmail.com>
2022-10-25 20:31:08 -0500

    libreria

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,16 @@
+const purgecss = require('@fullhuman/postcss-purgecss')
+
+
 module.exports = {
     plugins: {
+      
        tailwindcss: {},
        autoprefixer: {},
+       purgecss({
+        content: ['./**/*.html']
+      })
+      require('cssnano')({
+        preset: 'default',
+      }),
     }
   }
